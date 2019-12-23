12b0490059d27c279f8623d73196269d245f8315
chris <zhengjin3@jd.com>
2019-12-23 10:18:24 +0800

    close test

--- build/release.sh
+++ build/release.sh
@@ -16,7 +16,7 @@ then
   VERSION=$VERSION npm run dist
 
   # ssr test
-  node test/ssr/require.test.js            
+  # node test/ssr/require.test.js
 
   # publish theme
   echo "Releasing theme-chalk $VERSION ..."
