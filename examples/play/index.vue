a65b40bf4ad6f5407f513f287a2ba618d2da4e95
chris <zhengjin3@jd.com>
2019-12-27 15:15:16 +0800

    button done

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div style="margin: 20px;">
     <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <el-button type="danger" style="width: 100%" size="large" round>搜索</el-button>
+    <div>
+      <el-button type="danger" style="width: 100%" size="large" round disabled>搜索</el-button>
+    </div>
   </div>
 </template>
 
