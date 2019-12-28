08dd2dfbe9095f9cd5924b5a5f7b4fc58bf871e8
chris <zhengjin3@jd.com>
2019-12-28 16:19:13 +0800

    change version

--- src/index.js
+++ src/index.js
@@ -174,21 +174,21 @@ const install = function(Vue, opts = {}) {
     Vue.component(component.name, component);
   });
 
-  Vue.use(InfiniteScroll);
-  Vue.use(Loading.directive);
+  // Vue.use(InfiniteScroll);
+  // Vue.use(Loading.directive);
 
   Vue.prototype.$ELEMENT = {
     size: opts.size || '',
     zIndex: opts.zIndex || 2000
   };
 
-  Vue.prototype.$loading = Loading.service;
-  Vue.prototype.$msgbox = MessageBox;
-  Vue.prototype.$alert = MessageBox.alert;
-  Vue.prototype.$confirm = MessageBox.confirm;
-  Vue.prototype.$prompt = MessageBox.prompt;
-  Vue.prototype.$notify = Notification;
-  Vue.prototype.$message = Message;
+  // Vue.prototype.$loading = Loading.service;
+  // Vue.prototype.$msgbox = MessageBox;
+  // Vue.prototype.$alert = MessageBox.alert;
+  // Vue.prototype.$confirm = MessageBox.confirm;
+  // Vue.prototype.$prompt = MessageBox.prompt;
+  // Vue.prototype.$notify = Notification;
+  // Vue.prototype.$message = Message;
 
 };
 
@@ -198,12 +198,12 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '2.13.0',
+  version: '1.0.18',
   locale: locale.use,
   i18n: locale.i18n,
   install,
   CollapseTransition,
-  Loading,
+  // Loading,
   Pagination,
   Dialog,
   Autocomplete,
