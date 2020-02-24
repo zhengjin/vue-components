1820c791988c70a5fed697db9717c6c0c6ee3b6b
chris <zhengjin3@jd.com>
2020-02-24 14:40:59 +0800

    [build] 0.0.42

--- src/index.js
+++ src/index.js
@@ -199,7 +199,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '0.0.41',
+  version: '0.0.42',
   // locale: locale.use,
   // i18n: locale.i18n,
   install,
