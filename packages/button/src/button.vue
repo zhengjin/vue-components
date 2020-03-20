f41b623c86954c6e9d65793c334e856e33d170e1
chris <zhengjin3@jd.com>
2020-03-20 17:13:07 +0800

    add hammer tap event

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,11 +1,31 @@
 <template>
-  <button
-    class="el-button"
-    @click="handleClick"
-    :disabled="buttonDisabled || loading"
-    :autofocus="autofocus"
-    :type="nativeType"
-    :class="[
+  <!--<button-->
+    <!--class="el-button"-->
+    <!--@click="handleClick"-->
+    <!--:disabled="buttonDisabled || loading"-->
+    <!--:autofocus="autofocus"-->
+    <!--:type="nativeType"-->
+    <!--:class="[-->
+      <!--type ? 'el-button&#45;&#45;' + type : '',-->
+      <!--buttonSize ? 'el-button&#45;&#45;' + buttonSize : '',-->
+      <!--{-->
+        <!--'is-disabled': buttonDisabled,-->
+        <!--'is-loading': loading,-->
+        <!--'is-plain': plain,-->
+        <!--'is-round': round,-->
+        <!--'is-circle': circle-->
+      <!--}-->
+    <!--]"-->
+  <!--&gt;-->
+    <!--<i class="el-icon-loading" v-if="loading"></i>-->
+    <!--<i :class="icon" v-if="icon && !loading"></i>-->
+    <!--<span v-if="$slots.default"><slot></slot></span>-->
+  <!--</button>-->
+  <v-touch class="el-button" tag="button" @tap="handleClick"
+           :disabled="buttonDisabled || loading"
+           :autofocus="autofocus"
+           :type="nativeType"
+           :class="[
       type ? 'el-button--' + type : '',
       buttonSize ? 'el-button--' + buttonSize : '',
       {
@@ -15,14 +35,16 @@
         'is-round': round,
         'is-circle': circle
       }
-    ]"
-  >
+    ]">
     <i class="el-icon-loading" v-if="loading"></i>
     <i :class="icon" v-if="icon && !loading"></i>
     <span v-if="$slots.default"><slot></slot></span>
-  </button>
+  </v-touch>
 </template>
 <script>
+  import Vue from 'vue';
+  import VueTouch from 'vue-touch';
+  Vue.use(VueTouch, { name: 'v-touch' });
   export default {
     name: 'ElButton',
 
