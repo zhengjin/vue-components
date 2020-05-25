2e5f6d70fdc8bcc3cbdc885958099b4b78a47a86
chris <zhengjin3@jd.com>
2020-05-25 15:43:36 +0800

    revert package

--- src/utils/scrollbar-width.js
+++ src/utils/scrollbar-width.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
 
-let scrollBarWidth;
+var scrollBarWidth;
 
 export default function() {
   if (Vue.prototype.$isServer) return 0;
   if (scrollBarWidth !== undefined) return scrollBarWidth;
 
-  const outer = document.createElement('div');
+  var outer = document.createElement('div');
   outer.className = 'el-scrollbar__wrap';
   outer.style.visibility = 'hidden';
   outer.style.width = '100px';
@@ -14,14 +14,14 @@ export default function() {
   outer.style.top = '-9999px';
   document.body.appendChild(outer);
 
-  const widthNoScroll = outer.offsetWidth;
+  var widthNoScroll = outer.offsetWidth;
   outer.style.overflow = 'scroll';
 
-  const inner = document.createElement('div');
+  var inner = document.createElement('div');
   inner.style.width = '100%';
   outer.appendChild(inner);
 
-  const widthWithScroll = inner.offsetWidth;
+  var widthWithScroll = inner.offsetWidth;
   outer.parentNode.removeChild(outer);
   scrollBarWidth = widthNoScroll - widthWithScroll;
 
