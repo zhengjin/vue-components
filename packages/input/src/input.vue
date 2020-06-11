55a0bc7818a259e508cc5dba8ed506b39754796d
chris <zhengjin3@jd.com>
2020-06-11 20:01:03 +0800

    common input

--- packages/input/src/input.vue
+++ packages/input/src/input.vue
@@ -373,7 +373,7 @@
             if (_this.canScroll() && _this.getSysType('isAndroid')) {
               console.log('Android 键盘弹起啦！');
               // Android 键盘弹起后操作
-              document.body.style.marginBottom = '250px';
+              document.body.style.marginBottom = '250px';// 发现三星部分手机有弹起问题
 
               const activeElement = _this.getInput();
 
